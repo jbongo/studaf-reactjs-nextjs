e31280ae38b54097b12c0563866d1b52dcf52a19
jean philippe <bongojeanphilippe@gmail.com>
2020-08-27 19:25:02 +0200

    deploy on vercel

--- pages/App.js
+++ pages/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+// import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 
 import "./assets/css/bootstrap-grid.css"
