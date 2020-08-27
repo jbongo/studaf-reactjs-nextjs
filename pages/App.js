62b792886367288439131aeffac18b4d4c0436d3
jean philippe <bongojeanphilippe@gmail.com>
2020-08-27 19:25:50 +0200

    deploy on vercel

--- pages/App.js
+++ pages/App.js
@@ -2,37 +2,7 @@ import React, { useEffect } from 'react';
 // import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 
-import "./assets/css/bootstrap-grid.css"
-import "./assets/css/icons.css"
-import "./assets/css/animate.min.css"
-import "./assets/css/style.css"
-import "./assets/css/responsive.css"
-import "./assets/css/chosen.css"
-import "./assets/css/colors/colors.css"
-import "./assets/css/bootstrap.css"
 
-
-import Home from './pages/Home/Home'
-import Connexion from './pages/Authentification/Connexion'
-import Inscription from './pages/Authentification/Inscription'
-import Contact from './pages/Contact/Contact'
-import TopMenuHome from './pages/TopMenu/TopMenuHome';
-import Footer from './pages/Footer/Footer'
-import Blog from './pages/Blog/Blog';
-import Article from './pages/Blog/Article';
-import ProfileRecru from './pages/Recruteur/Profile';
-import Job from './pages/Recruteur/Job';
-import ResumeRecru from './pages/Recruteur/Resume';
-import AddJob from './pages/Recruteur/AddJob';
-import AlertsRecru from './pages/Recruteur/Alerte';
-import ChangePassword from './pages/Authentification/ChangePassword';
-
-import ProfileCandi from './pages/Candidat/Profile';
-import Favoris from './pages/Candidat/Favori';
-import AlertsCandi from './pages/Candidat/Alert';
-import ResumeCandi from './pages/Candidat/Resume';
-import Candidatures from './pages/Candidat/Candidature';
-import Offre from './pages/Offres/Offre';
 
 
 // class App extends React.Component {
